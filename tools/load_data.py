ce8689b57e85442b5a103f83eb24707c5a6d5a94
Adam Kariv <adam.kariv@gmail.com>
2011-06-17 16:57:08 +0300

    ++

--- tools/load_data.py
+++ tools/load_data.py
@@ -29,53 +29,56 @@ if __name__=="__main__":
     saved_stat = shelve.open('saved_state')
     
     to_process = [ '/data' ]
+
+    try:    
+        while len(to_process) > 0:
+                   
+            relpath = to_process.pop(0)
+            
+            desc = os.path.join(DATA_ROOT, relpath[1:], DESCRIPTION_FILE )
     
-    while len(to_process) > 0:
-               
-        relpath = to_process.pop(0)
-        
-        desc = os.path.join(DATA_ROOT, relpath[1:], DESCRIPTION_FILE )
-
-        print "description file %s" % desc        
-        description = json.loads(file(desc).read())
-
-        path, slug = os.path.dirname(relpath), os.path.basename(relpath)
-        print ">>>>>>>",path,slug,description
-        DBLoader.new_item(path,slug,description)
-        
-        if description.has_key('subcatalogs'):
-            to_process.extend( [ os.path.join( relpath, x) for x in description['subcatalogs'] ] )
-        elif description.has_key('datafile'):
-            filename = os.path.join( DATA_ROOT, relpath[1:], description['datafile'] )
-            filename = str(filename)
+            print "description file %s" % desc        
+            description = json.loads(file(desc).read())
+    
+            path, slug = os.path.dirname(relpath), os.path.basename(relpath)
+            print ">>>>>>>",path,slug,description
+            DBLoader.new_item(path,slug,description)
             
-            file_stat, to_skip = saved_stat.get(filename,(0,0))
+            if description.has_key('subcatalogs'):
+                to_process.extend( [ os.path.join( relpath, x) for x in description['subcatalogs'] ] )
+            elif description.has_key('datafile'):
+                filename = os.path.join( DATA_ROOT, relpath[1:], description['datafile'] )
+                filename = str(filename)
+                
+                file_stat, to_skip = saved_stat.get(filename,(0,0))
+                
+                fields = description['fields']   
+                    
+                print "data file %s" % filename
             
-            fields = description['fields']   
-                
-            print "data file %s" % filename
-        
-            loader = Loader.get_loader_for_filename(filename,fields)
-            if loader != None:
-                
-                if calc_stat(filename) != file_stat:
-                    to_skip = 0
-                    DBLoader.del_collection(relpath)
-
-                print "Processing %s, %s" % (relpath, description['datafile'] )
-                print "skipping %s records" % to_skip        
-
-                filename_stat = calc_stat(filename)
-                
-                num_rows = 0
-                saved_stat[filename] = filename_stat, to_skip
-                for slug,rec in loader.get_processed_rows():
-                    if num_rows < to_skip:
+                loader = Loader.get_loader_for_filename(filename,fields)
+                if loader != None:
+                    
+                    if calc_stat(filename) != file_stat:
+                        to_skip = 0
+                        DBLoader.del_collection(relpath)
+    
+                    print "Processing %s, %s" % (relpath, description['datafile'] )
+                    print "skipping %s records" % to_skip        
+    
+                    filename_stat = calc_stat(filename)
+                    
+                    num_rows = 0
+                    saved_stat[filename] = filename_stat, to_skip
+                    for slug,rec in loader.get_processed_rows():
+                        if num_rows < to_skip:
+                            num_rows += 1
+                            continue
+                        print "Loading: slug = %s, row = %s" % (slug, rec)
+                        DBLoader.new_item(relpath,slug,rec)
                         num_rows += 1
-                        continue
-                    print "Loading: slug = %s, row = %s" % (slug, rec)
-                    DBLoader.new_item(relpath,slug,rec)
-                    num_rows += 1
-                    saved_stat[filename] = calc_stat(filename), num_rows
-
+                        saved_stat[filename] = calc_stat(filename), num_rows
+    except:
+        pass
+    
     os.unlink('/tmp/mylock')
